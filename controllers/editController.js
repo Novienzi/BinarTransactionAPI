f00a5b4d6602c17a27e41f837808de51094e3b82
JatraMaya <ahmad.f.iksan@gmail.com>
2020-10-25 14:10:22 +0800

    add modification to dbconnection, addController, and editController

--- controllers/editController.js
+++ controllers/editController.js
@@ -1,13 +1,13 @@
-const db = require("../connections/dbConnection")
+const db = require("../connections/dbConnection");
 
 /**
  * Edit data
- * 
+ *
  * Usage example:
- * 
+ *
  *    edit('transaction', "1", { nominal: 4000 })
  *    // 👆 edit data by id "1" to having 4000 nominal
- * 
+ *
  * @param {String} tableName choose table
  * @param {String} id data id to update
  * @param {Object} data new data to update
@@ -15,32 +15,42 @@ const db = require("../connections/dbConnection")
  * @returns {Boolean} Retuns `false` if id wasn't string, not found, or data object keys was lacking
  */
 function editData(tableName, id, data) {
-  if (!id) return false
-  if (typeof id !== 'string') return false
-  const searchResult = db.get(tableName)
-    .find({ id })
-    .value()
-  if (searchResult) {
-    let shapedData;
-    data.id = id
-    if (tableName == 'transaction') {
-      shapedData = shapeObject(data, transactionModel)
+    if (!id) return false;
+    if (typeof id !== "string") return false;
+    const searchResult = db
+        .get(tableName)
+        .find({ id })
+        .value();
+    if (searchResult) {
+        let shapedData;
+        data.id = id;
+        if (tableName == "transaction") {
+            shapedData = shapeObject(data, transactionModel);
+        }
+        if (tableName == "user") {
+            shapedData = shapeObject(data, userModel);
+        }
+        if (tableName == "vehicle") {
+            shapedData = shapeObject(data, vehicleModel);
+        }
+        if (tableName == "customer") {
+            shapedData = shapeObject(data, customerModel);
+        }
+        if (tableName == "admin") {
+            shapedData = shapeObject(data, adminModel);
+        }
+
+        if (!shapedData) return false;
+
+        db.get(tableName)
+            .find(id)
+            .assign(shapedData)
+            .write();
+
+        return data;
+    } else {
+        return false;
     }
-    if (tableName == 'user') {
-      shapedData = shapeObject(data, userModel)
-    }
-
-    if (!shapedData) return false
-
-    db.get(tableName)
-      .find(id)
-      .assign(shapedData)
-      .write()
-
-    return data
-  } else {
-    return false
-  }
 }
 
-module.exports = editData
+module.exports = editData;
