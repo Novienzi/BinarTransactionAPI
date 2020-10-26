be200ffb747a755a5eec94d4dec320f5d601e61c
novienzi <novienghi@gmail.com>
2020-10-26 14:27:22 +0700

    clean add transaction

--- routes/transaction/addTransaction.js
+++ routes/transaction/addTransaction.js
@@ -3,16 +3,9 @@ const express = require('express')
 const app = express.Router()
 const { verifyJwt } = require('../../middlewares/jwtMiddleware')
 const addData = require('../../controllers/addController')
-const getData = require('../../controllers/getController')
 
 app.post('/transaction', verifyJwt, function (req, res) {
-    // const dataCustomer = getData('customer')
-    // const dataAdmin = getData('admin')
-    // const dataVehicle = getData('vehicle')
     const body = req.body
-    // body.customerId = dataCustomer[0].userId
-    // body.adminId = dataAdmin[0].userId
-    // body.vehicleId = dataVehicle[0].id
     const result = addData('transaction', body)
     if (result) {
         res.send(result)
